2a964f6c5e117d6b31c4be0ecbe55e683734d281
Prakhar Kochar <62941019+Prakhar2100@users.noreply.github.com>
2021-11-14 19:52:13 +0530

    connection to the database

---
+++ connection/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $servername   =  "localhost";
+    $username     =  "root";
+    $password     =  ":)";
+
+    $conn = mysqli_connect($servername, $username, $password);
+
+    if (!$conn) {
+        die("Sorry we failed to connect: " . mysqli_connect_error());
+    } 
+?>
